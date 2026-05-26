7e7f2eaa638d1a9b476e0ad4f660d5f5cb4074d5
Melisa Sabatino <sabatinomelisa@gmail.com>
2026-05-26 15:56:30 -0300

    Initial commit

---
+++ ClubManager/Program.cs
@@ -0,0 +1,38 @@
+﻿using Patrones.Singleton.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClubManager
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Usuario usuario = new Usuario();
+            usuario.Username = "prueba";
+            usuario.Password = "prueba";
+
+            try
+            {
+                SessionManager.Login(usuario);
+                SessionManager u = SessionManager.GetInstance;
+                SessionManager.Logout(usuario);
+
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ Patrones.Singleton.Core/SessionManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Patrones.Singleton.Core
+{
+    public class SessionManager
+    {
+        private static object _lock = new object();
+
+        private static SessionManager session;
+
+        Usuario Usuario { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public static SessionManager GetInstance
+        {
+            get
+            {
+                if (session == null) throw new Exception("Sesión no iniciada");
+                return session;
+            }
+        }
+
+        public static void Login(Usuario usuario)
+        {
+            lock (_lock)
+            {
+                if (session == null)
+                {
+                    session = new SessionManager();
+                    session.Usuario = usuario;
+                    session.FechaInicio = DateTime.Now;
+
+                }
+                else
+                {
+                    throw new Exception("Sesión ya iniciada");
+                }
+            }
+
+        }
+
+        public static void Logout()
+        {
+            lock (_lock)
+            {
+                if (session != null)
+                {
+                    session = null;
+                }
+                else
+                {
+                    throw new Exception("Sesion no iniciada");
+                }
+            }
+
+        }
+        private SessionManager() 
+        {
+        
+        }
+
+    }
+}
